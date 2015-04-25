30a4d0a8a82d4b9b110fd0a7535a698cf9b33a1d
Jessica He <jessheisme@gmail.com>
2015-04-25 11:11:27 -0700

    question view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  def new
+  	@question = Question.new
+  end
+
+  def create
+  	@question = Question.create(question_params)
+  	@question.score = 0
+  end
+
+  def vote
+  	@question.score += 1
+  end
 end
