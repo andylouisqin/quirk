52acfc008c1771552134b98cf047b6a52b586e70
Andy Qin <andrewlqin@gmail.com>
2015-04-26 21:11:36 -0700

    made a CRUD for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,18 +1,46 @@
 class RoomsController < ApplicationController
-    def home
+    def index
         @rooms = Room.all
+    end
+
+    def show
+        @room = Room.find(params[:id])
+    end
+
+    def create
+        @room = Room.new(room_params)
+
+        if @room.save
+            redirect_to @room # WHAT TO DO
+        else
+            flash[:error] = @room.errors.full_messages
+            render "new"
+        end
     end
 
     def new
         @room = Room.new
     end
 
-    def create
-        @room = Room.new(room_params)
-        redirect_to @room
+    def edit
+        @room = Room.find(params[:id])
     end
 
-    def show
+    def update
         @room = Room.find(params[:id])
+        @room.update(room_params)
+    end
+
+    def destroy
+        room = Room.find(params[:id])
+        room.destroy
+
+        redirect_to "/rooms"
+    end
+
+    private
+        
+    def room_params
+        params.require(:room).permit(:name, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-    root "rooms#home"
-    get "new", to:"rooms#new"
-    get "rooms/:id", to: "rooms#show"
-    post "rooms", to: "rooms#create"
+    # root "rooms#home"
+    # get "new", to: "rooms#new"
+    # get "rooms/:id", to: "rooms#show", as: "room"
+    # post "rooms", to: "rooms#create"
+    root "rooms#index"
+    resources :rooms
+
+    # get "rooms/:id/ask", to: "questions#new"
 end
